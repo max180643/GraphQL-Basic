812c95ebfba70dbc039650ca38f4292652483100
max180643 <max180643@gmail.com>
2021-06-19 01:09:07 +0700

    :rocket: connect to database

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
 import server from "./server";
 
-const app = express();
+dotenv.config();
 
-const PORT = 4444;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
+const PORT = process.env.PORT;
 
-server.applyMiddleware({ app });
+const createServer = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${DB_USER}:${DB_PASSWORD}@graphql-basic.entvt.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    );
 
-app.listen({ port: PORT }, () => {
-  console.log(
-    `Server running at http://localhost:${PORT}${server.graphqlPath}`
-  );
-});
+    const app = express();
+
+    server.applyMiddleware({ app });
+
+    app.listen({ port: PORT }, () => {
+      console.log(
+        `Server running at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+createServer();
--- src/resolvers/resolvers.js
+++ src/resolvers/resolvers.js
@@ -1,37 +1,47 @@
-// Mockup database
-const users = [
-  {
-    id: "1",
-    name: "A",
-  },
-  {
-    id: "2",
-    name: "B",
-  },
-  {
-    id: "3",
-    name: "C",
-  },
-];
+import User from "../models/user";
 
-const me = users[0];
+// Mockup database
+// const users = [
+//   {
+//     id: "1",
+//     name: "A",
+//   },
+//   {
+//     id: "2",
+//     name: "B",
+//   },
+//   {
+//     id: "3",
+//     name: "C",
+//   },
+// ];
+
+// const me = users[0];
+
+// args มาจาก argument ใน schema.graphql
+
+const Query = {
+  // me: (parent, args, context, info) => {
+  //   return me;
+  // },
+  user: (parent, args, context, info) => {
+    return User.findById(args.id);
+  },
+  users: (parent, args, context, info) => {
+    return User.find({});
+  },
+};
+
+const Mutation = {
+  signup: async (parent, args, context, info) => {
+    const newUser = await User.create(args);
+    return newUser;
+  },
+};
 
 const resolvers = {
-  Query: {
-    me: (parent, args, context, info) => {
-      return me;
-    },
-    user: (parent, args, context, info) => {
-      const id = args.id;
-      const user = users.find((u) => {
-        return u.id === id;
-      });
-      return user;
-    },
-    users: (parent, args, context, info) => {
-      return users;
-    },
-  },
+  Query,
+  Mutation,
 };
 
 export default resolvers;
